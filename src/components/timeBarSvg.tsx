f4eb3c932776f7e014793c10d6de47e25f3c2617
ichizokichinosuke <nuuuukemw@gmail.com>
2023-06-21 00:43:06 +0900

    レイアウト完成

---
+++ src/components/timeBarSvg.tsx
@@ -0,0 +1,8 @@
+export default function TimeBarSvg() {
+    return (
+        <svg width="1194" height="18" viewBox="0 0 1194 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="1194" height="18" fill="white"/>
+            <path d="M0 2C0 0.89543 0.895431 0 2 0H1192C1193.1 0 1194 0.895431 1194 2V18H0V2Z" fill="#81D4FA"/>
+        </svg>
+    )
+}
