39b8dc84ec8be3510b3e5d4347fce546296dc3a2
ichizokichinosuke <nuuuukemw@gmail.com>
2023-06-21 01:05:02 +0900

    縦配置整理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,40 +31,38 @@ export default function Home() {
   return (
     <main>
       <TimeBarSvg />
-      <section className={`flex min-h-screen flex-col items-center justify-between p-24 bg-${bgColor}`}>
-      <h2 className={`${textColor} text-[202px] font-bold`}>20:00</h2>
-      <section>
-        <div className="flex flex-row items-center justify-between">
-          <h3 className="text-6xl font-bold mr-10">宮薗</h3>
-          <div onClick={() => setCount(count+1)} className="mx-3">
-            <PlusSvg />
+      <section className={`flex min-h-screen flex-col items-center justify-between p-12 bg-${bgColor}`}>
+        <h2 className={`${textColor} text-[202px] font-bold`}>20:00</h2>
+        <section>
+          <div className="flex flex-row items-center justify-between mb-14">
+            <h3 className="text-6xl font-bold mr-10">宮薗</h3>
+            <div onClick={() => setCount(count+1)} className="mx-3">
+              <PlusSvg />
+            </div>
+            <strong className={`${textColor} text-6xl font-bold mx-6`}>{count}</strong>
+            <div onClick={() => setCount(count-1)} className="mx-3">
+              <MinusSvg />
+            </div>
+            <strong className={`${textColor} text-6xl font-bold ml-10`}>杯</strong>
           </div>
-          <strong className={`${textColor} text-6xl font-bold mx-6`}>{count}</strong>
-          <div onClick={() => setCount(count-1)} className="mx-3">
-            <MinusSvg />
+          <div className="flex flex-row items-center justify-between mb-14">
+            <h3 className="text-6xl font-bold mr-10">阿多</h3>
+            <div onClick={() => setCount(count+1)} className="mx-3">
+              <PlusSvg />
+            </div>
+            <strong className={`${textColor} text-6xl font-bold mx-6`}>{count}</strong>
+            <div onClick={() => setCount(count-1)} className="mx-3">
+              <MinusSvg />
+            </div>
+            <strong className={`${textColor} text-6xl font-bold ml-10`}>杯</strong>
           </div>
-          <strong className={`${textColor} text-6xl font-bold ml-10`}>杯</strong>
-        </div>
-        <div className="flex flex-row items-center justify-between">
-          <h3 className="text-6xl font-bold mr-10">阿多</h3>
-          <div onClick={() => setCount(count+1)} className="mx-3">
-            <PlusSvg />
+        </section>
+        <section className="mb-10">
+          <div className="flex flex-col items-center">
+            <h4 className={`text-6xl ${textColor} font-bold mb-4`}>00:00:00</h4>
+            <h4 className={`text-4xl ${textColor} font-bold`}>経過</h4>
           </div>
-          <strong className={`${textColor} text-6xl font-bold mx-6`}>{count}</strong>
-          <div onClick={() => setCount(count-1)} className="mx-3">
-            <MinusSvg />
-          </div>
-          <strong className={`${textColor} text-6xl font-bold ml-10`}>杯</strong>
-        </div>
-      </section>
-      <section>
-        <div className="flex flex-col items-center">
-          <h4 className={`text-6xl ${textColor} font-bold`}>00:00:00</h4>
-          <h4 className={`text-4xl ${textColor} font-bold`}>経過</h4>
-        </div>
-      </section>
-      <div>
-      </div>
+        </section>
       </section>
     </main>
   )
